5bbc6257655f07093e8800404e9f15fa1ff14342
hmehryari <87432961+hmehryari@users.noreply.github.com>
2021-07-31 14:31:49 +0000

    Add files via upload

--- week2/passwordcheck.sh
+++ week2/passwordcheck.sh
@@ -1,4 +1,20 @@
 #!/bin/bash
+read -p "type a folder name: " folderName
+mkdir "$folderName"
+read -sp "Password :" user_pass
+echo
+echo $user_pass | sha256sum >> ~/scripts/portfolio/week2/$folderName/secret.txt
 
-read -sp ' secret password: ' pass_var
-echo "$pass_var" | sha256sum > ~/scripts/portfolio/week2/secret.txt
+var_a=`cat secret.txt`
+read -sp "second password :" var_b
+echo
+echo $var_b | sha256sum >> ~/scripts/portfolio/week2/$folderName/pass_hash
+
+if var_a=pass_hash 
+then
+    echo "Your access is granted!"
+else 
+    echo "Your access is denied!"
+fi
+
+exit0
