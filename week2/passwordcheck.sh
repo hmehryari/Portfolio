5903384b6a5e005070f51d43a477c1f7b045e825
hmehryari <87432961+hmehryari@users.noreply.github.com>
2021-08-01 10:52:31 +0000

    Add files via upload

--- week2/passwordcheck.sh
+++ week2/passwordcheck.sh
@@ -5,16 +5,18 @@ read -sp "Password :" user_pass
 echo
 echo $user_pass | sha256sum >> ~/scripts/portfolio/week2/$folderName/secret.txt
 
-var_a=`cat secret.txt`
+var_a=`cat ~/scripts/portfolio/week2/$folderName/secret.txt`
 read -sp "second password :" var_b
 echo
-echo $var_b | sha256sum >> ~/scripts/portfolio/week2/$folderName/pass_hash
 
-if var_a=pass_hash 
-then
+# echo $var_b | sha256sum >> ~/scripts/portfolio/week2/$folderName/pass_hash
+
+var_c=`echo $var_b | sha256sum`
+
+if [ "$var_a" = "$var_c" ]; then
     echo "Your access is granted!"
 else 
     echo "Your access is denied!"
 fi
 
-exit0
+exit 0
